8c41128c8075745899bffb680ad47099af605ba1
Gothu <mayovwhite@gmail.com>
2020-04-01 18:48:11 +0900

    post-pay

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,9 +26,22 @@ class ItemsController < ApplicationController
   end
 
   def pay
+    Payjp.api_key = "sk_test_1ba767c5bffca296748263f9"
+    Payjp::Charge.create(
+    amount: 1200,
+    card: params['payjp-token'],
+    currency: 'jpy'
+    )
+    redirect_to action: :done
   end
 
   def done
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :text, :size, :prefecture, :status, :deliveryfee, :deliveryday).merge(user_id: current_user.id)
   end
 
 end
