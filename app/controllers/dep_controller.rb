01bdd66399244db170bbff356c541885a19fa844
ryuichi-yamamoto <yamamotoryuichi0802@gmail.com>
2020-04-01 11:52:35 +0900

    database.create

---
+++ app/controllers/dep_controller.rb
@@ -0,0 +1,4 @@
+class DepController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root 'dep#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'items#index'
   resources :items
 end
