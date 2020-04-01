84b404acc7fa7ca11736bae8ffaeb212eb7d9b37
makoto1026 <trsmmkt@gmail.com>
2020-04-01 17:36:12 +0900

    newView_fix_commit2

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,8 @@ class Item < ApplicationRecord
   belongs_to :brand
   belongs_to :category
   
+
+  enum status: { "新品、未使用" => 0, "未使用に近い" => 1, "目立った傷や汚れなhし" => 2,
+                 "やや傷や汚れあり" => 3,  "傷や汚れあり" => 4, "全体的に状態が悪い" => 5}
+
 end
