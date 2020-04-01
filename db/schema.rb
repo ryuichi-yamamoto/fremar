a747f03c93e389bf7326e4d0b54e3e95e603bd50
makoto1026 <trsmmkt@gmail.com>
2020-04-01 18:57:01 +0900

    imagesUpload_charenge_commit_false

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2020_04_01_093325) do
 
-ActiveRecord::Schema.define(version: 2020_04_01_015336) do
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name", null: false
     t.string "last_name", null: false
@@ -69,6 +69,11 @@ ActiveRecord::Schema.define(version: 2020_04_01_015336) do
   create_table "p_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
     t.string "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "statuses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
