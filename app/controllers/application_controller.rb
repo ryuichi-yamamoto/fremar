4c882773936b221d54c45ed5b003c56f6a4161d8
ryuichi-yamamoto <yamamotoryuichi0802@gmail.com>
2020-04-01 12:42:33 +0900

    dep.create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def production?
+    Rails.env.production?
+  end
+
+  def basic_auth , if: :production?
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.credentials[:basic_auth][:user] &&
+      password == Rails.application.credentials[:basic_auth][:pass]
+    end
+  end
 end
