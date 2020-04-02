e7cbe16c70a33254711a76390a34ba3c29f9728a
yumi-o-0428 <ochabi0428@gmail.com>
2020-04-02 14:27:20 +0900

    fix_toppage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_04_01_170835) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
