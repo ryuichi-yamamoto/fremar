157aaaabe04918b4f8841a2f42460a3e3b73b70e
yumi-o-0428 <ochabi0428@gmail.com>
2020-04-03 15:25:14 +0900

    exchange-category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_04_03_044037) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
