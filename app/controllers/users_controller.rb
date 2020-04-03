74075cf952b0e1f4b8df8a20dd4963b52ef0d745
ryuichi-yamamoto <yamamotoryuichi0802@gmail.com>
2020-04-03 15:07:53 +0900

    user.edit.new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,17 +14,29 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @users = User.find(params[:id])
-    @addresses = Addrerre.find(params[:id])
+    @user = User.find(params[:id])
+    # @addresses = Addrerre.find(params[:id])
   end
 
   def update
+    if current_user.update(user_params)
+      redirect_to users_path
+      sign_in(current_user, bypass: true)
+    else
+      redirect_to users_path
+    end
   end
   
   def destroy
   end
 
   def done
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :first_name, :last_name, :first_name_hiragana, :last_name_hiragana, :birth_year, :birth_month, :birth_day, :email, :password, :password_confirmation, :status, :deleted_at)
   end
 
 end
