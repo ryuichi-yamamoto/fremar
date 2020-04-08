2c35b26d075d4f567a0b39b0f992f010a68bf719
ryuichi-yamamoto <yamamotoryuichi0802@gmail.com>
2020-04-08 10:26:49 +0900

    ko

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,50 +1,21 @@
-unless Rails.env.development? || Rails.env.test?
-  require 'carrierwave/storage/abstract'
-  require 'carrierwave/storage/file'
-  require 'carrierwave/storage/fog'
 
-  CarrierWave.configure do |config|
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  else
+    config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-      provider:              'AWS',
+      provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1',
+      region: 'ap-northeast-1'
     }
-  
-    # 環境ごとにS3のバケットを指定
-    case Rails.env
-      when 'production'
-        config.fog_directory  = 'fremar-product-image'
-        config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/fremar-product-image'
-  
-      when 'development'
-        config.fog_directory  = 'fremar-product-image'
-        config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/fremar-product-image'
-  
-      when 'test'
-        config.fog_directory  = 'fremar-product-image'
-        config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/fremar-product-image'
-    end
+    config.fog_directory  = 'fremar-product-image'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/fremar-product-image'
   end
 end
-
-
-
-#   CarrierWave.configure do |config|
-#     if Rails.env.development? || Rails.env.test?
-#       config.storage = :file
-#     else
-#       config.storage = :fog
-#       config.fog_provider = 'fog/aws'
-#       config.fog_credentials = {
-#         provider: 'AWS',
-#         aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-#         aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-#         region: 'ap-northeast-1'
-#       }
-#       config.fog_directory  = 'fremar-product-image'
-#       config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/fremar-product-image'
-#     end
-#   end
-# end
