6905bad775208a6021de23eb11b1a9d84d4a73ce
ryuichi-yamamoto <yamamotoryuichi0802@gmail.com>
2020-04-08 12:24:25 +0900

    ベーシック認証実装

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,10 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+server "(18.178.33.162)", user: "ec2-user", roles: %w{app db web}
 
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
 
 # role-based syntax
 # ==================
