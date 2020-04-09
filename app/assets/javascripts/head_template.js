f36cc90ae4ee2693200f4ea4cb70acec15f688c8
makoto1026 <trsmmkt@gmail.com>
2020-04-09 15:15:29 +0900

    head_template_fix_commit

--- app/assets/javascripts/head_template.js
+++ app/assets/javascripts/head_template.js
@@ -4,10 +4,10 @@ $(document).on('turbolinks:load', function(){
   });
   $(".header-grp__header__lists__leftbox__category li ul").hide();
   $(".header-grp__header__lists__leftbox__category li").hover(function() {
-    $(">ul:not(:animated)", this).stop(true, true).slideDown("fast");
+    $(">ul:not(:animated)", this).stop(true, true).slideDown(1);
     $(">a", this).addClass("active");
   }, function() {
-    $(">ul:not(:animated)", this).stop(true, true).slideUp("fast");
+    $(">ul:not(:animated)", this).stop(true, true).slideUp(1);
     $(">a", this).removeClass("active");
   });
 });
