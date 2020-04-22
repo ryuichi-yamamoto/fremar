bbe3e839480e5143f48d15e8853a939691eb8a20
makoto1026 <trsmmkt@gmail.com>
2020-04-22 17:46:32 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -23,7 +23,7 @@ class Item < ApplicationRecord
   validates_associated :images
   validates :images,      presence: true
   validates :user_id,     presence: true
-  validates :category_id, presence: true
+  validates :category,    presence: true
   validates :condition,   presence: true
 
   def self.search(search)
